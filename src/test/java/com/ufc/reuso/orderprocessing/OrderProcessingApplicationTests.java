5145fa0124f89fbc37bf3cf5ff1862704a547639
Christian Estevam <christianestevamb@gmail.com>
2025-02-23 17:12:15 -0300

    init: setup project

---
+++ src/test/java/com/ufc/reuso/orderprocessing/OrderProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc.reuso.orderprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
